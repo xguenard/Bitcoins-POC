6a10ca30712c340d6da16c89139786f76b8020d0
Guenard, Xavier <Xavier.Guenard@misys.com>
2015-09-24 16:27:20 +0200

    small modif

--- text_saver.py
+++ text_saver.py
@@ -20,9 +20,31 @@ class TestUser():
         print( self.priv.is_compressed)
 
 
-   
 
+class Message():
+    def __init__(self, path, tx_msg_size):
+        self.path = path
+        self.packets = []
+        self.msg_size = tx_msg_size
 
+    def FillPackets(self):
+        """Fill the packets list with strings of size msg_size"""
+        reader = open( self.path , "r")
+        tmp = reader.readlines()
+        buff = ""
+        place = self.msg_size
+
+        for s in tmp:
+            if( len(s) >=  place ):
+                buff += s[0:place]
+                place = 0 
+            else :
+                buff += s
+                place -= len(s)
+            if( place == 0):
+                self.packets.append(buff)
+                buff = ""
+                place = self.msg_size
 
 
 def main():
