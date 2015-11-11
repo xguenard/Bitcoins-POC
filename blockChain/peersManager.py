dc9022812920eb583b618ed10f742dd1e25ef5b3
xguenard <xavier.guenard@gmail.com>
2015-11-11 17:09:37 +0100

    Pc change

--- blockChain/peersManager.py
+++ blockChain/peersManager.py
@@ -1,12 +1,14 @@
-import Queue
+import queue
+import select
+import threading
+import unittest
 
-
-class PeersManager(Threading.Thread):
+class PeersManager(threading.Thread):
     """Have to add locks"""
     def __init__(self, queue):
         super().__init__()
-        self.peer_q = Queue.Queue()
-        self.message_q = Queue.Queue()
+        self.peer_q = queue.Queue()
+        self.message_q = queue.Queue()
 
         self.active_peers = []                      #Active peer = active connection
         self.message_q = ["Hello", "World"]
@@ -14,18 +16,41 @@ class PeersManager(Threading.Thread):
         self.number_peers = 0
 
     def ProcessPeerQ(self):
-        while !self.peer_queue.empty() and self.number_peers <= self.max_peers:
+        """Adding peers to the active peers list"""
+        while not self.peer_queue.empty() and self.number_peers <= self.max_peers:
             peer = self.peer_queue.get()
             self.active_peers.append( peer )
             self.number_peers += 1
             self.queue.task_done()
 
     def ProcessMessageQ(self, writable_peers ):
-        while !self.message_q.empty():
+        """Processing the message queue"""
+        while not self.message_q.empty():
             msg = self.message_q.get()
             for peer in writable_peers:
                 peer.send(msg) 
             self.message_q.task_done()
 
     def run(self):
-        self.ProcessPeerQ()
+        """Peer management using select"""
+
+        while True:
+            self.ProcessPeerQ()
+
+
+
+
+
+class PeersManagerTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_threads(self):
+        t = PeersManager()
+        
+    
+
+
+
+if __name__ == "__main__":
+    unittest.main()
